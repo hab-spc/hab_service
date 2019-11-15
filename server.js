0fd9faf476efd73e22fdc502a6412fe58e719815
janAntu <randrus@ucsd.edu>
2019-11-15 13:00:54 -0800

    Added basic get and push API calls

--- server.js
+++ server.js
@@ -39,5 +39,65 @@ router.route('/imgs')
 // register routes
 app.use('/api', router);
 
+const sqlite3 = require('sqlite3').verbose();
+const openDB = (dbPath) => {
+    let db = new sqlite3.Database(dbPath, (err) => {
+        if (err) {
+        return console.error(err.message);
+        }
+        console.log('Connected to the in-memory SQlite database.');
+    });
+    return db;
+}
+
+const closeDB = (db) => {
+    db.close((err) => {
+        if (err) {
+          return console.error(err.message);
+        }
+      });
+};
+
+// get images for that date time range from the db
+router.get('/api/imgs/:date', (req, res) => {
+    const db = openDB(dbPath);
+    const sql = 'SELECT DISTINCT IMG_FNAME FROM IMAGES WHERE IMG_DATE = ?';
+    const params = [req.params.date];
+    db.get(sql, params, (err, row) => {
+        if (err) {
+          res.status(400).json({"error":err.message});
+          return;
+        }
+        res.json({
+            "message":"success",
+            "data":row
+        })
+    });
+    closeDB(db);
+});
+
+// update annotations for images specified by json object
+router.post("/api/imgs/", (req, res, next) => {
+    const errors=[]
+    const data = {
+        fname: req.body.fname,
+        label: req.body.label
+    }
+    const sql ='INSERT INTO IMAGES (IMG_FNAME, HMN_LBL) VALUES (?,?)'
+    const params =[data.fname, data.label]
+    db.run(sql, params, function (err, result) {
+        if (err){
+            res.status(400).json({"error": err.message})
+            return;
+        }
+        res.json({
+            "message": "success",
+            "data": data,
+            "id" : this.lastID
+        })
+    });
+    closeDB(db)
+})
+
 // Start the server
 app.listen(port);
