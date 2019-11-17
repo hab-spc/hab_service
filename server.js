cb8df11c83597a1c4eda3d4ae8a52c6094b28aee
igneel73 <igneel.kush73@gmail.com>
2019-11-17 13:39:46 -0800

    finish server

--- server.js
+++ server.js
@@ -7,6 +7,9 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const dbPath = "./test.db"
 
 // db utils
 const sqlite3 = require('sqlite3').verbose();
@@ -32,74 +35,45 @@ const closeDB = (db) => {
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-let port = process.env.PORT || 8080;
+let port = 3002;
 
-// ROUTES FOR OUR API
-let router = express.Router();
-
-// middleware for logging
-router.use((req,res,next) => {
-    console.log(res);
-    next();
-});
+// middleware for allowing CORS
+app.use(cors());
 
 // test
-router.get('/', (req, res) => {
+app.get('/', (req, res) => {
     res.json({message: 'welcome to our API'});
 });
-
-// register routes
-app.use('/api', router);
 
 // get images for that date time range from the db
 // date: yyyy-mm-dd
 // time: hh:mm:ss
-router.get('/api/imgs/:date', (req, res) => {
+app.get('/api/imgs/:date', (req, res) => {
 
     // process the dateTime string
     const dateTime = req.params.date;
-    const [dStart, dEnd, tStart, tEnd] = dateTime.split(" ");
+    const params = dateTime.split(" ");
 
     // query database for that range
     const db = openDB(dbPath);
-    const sql = 'SELECT * FROM IMAGES WHERE image_date > ? AND image_date < ? AND image_time > ? AND image_time < ?';
-    const params = "";
+    const sql = 'SELECT * FROM date_sampled WHERE image_date >= ? AND image_date < ? AND image_time >= ? AND image_time < ?';
 
     db.all(sql, params, (err, rows) => {
         if (err) {
           res.status(400).json({"error":err.message});
+          console.log(err.message);
           return;
         }
+        console.log(rows);
         res.json({
             "message":"success",
             "data":rows
-        })
+        });
     });
     closeDB(db);
 });
 
-// update annotations for images specified by json object
-router.post("/api/imgs/", (req, res, next) => {
-    const errors=[]
-    const data = {
-        fname: req.body.fname,
-        label: req.body.label
-    }
-    const sql ='INSERT INTO IMAGES (IMG_FNAME, HMN_LBL) VALUES (?,?)'
-    const params =[data.fname, data.label]
-    db.run(sql, params, function (err, result) {
-        if (err){
-            res.status(400).json({"error": err.message})
-            return;
-        }
-        res.json({
-            "message": "success",
-            "data": data,
-            "id" : this.lastID
-        })
-    });
-    closeDB(db)
-})
 
 // Start the server
 app.listen(port);
+console.log("listening on " + port);
